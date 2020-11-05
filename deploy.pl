a0c852ac8fa279506cbb1b146650d4d2a4f96122
mlori <mloria58@gmail.com>
2020-11-05 15:36:23 +0100

    pl script

---
+++ deploy.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl
+@containers = `docker ps`;
+
+foreach(@containers) {
+    if(m/(\w{12}).*8081->8081.*/) {
+        `docker stop $1`;
+        `docker rm $1`;
+    }
+}
+
+@images = `docker images`;
+
+foreach(@images) {
+    if(m/.*test_deploy.*(\w{12}).*/) {
+        `docker rmi $1`;
+    }
+}
+
+`docker build -t test_deploy -f Dockerfile .`;
+`docker run -dp 8081:8081 test_deploy`
